2b11eb8d707726c6058e0acbc6e2836c0ac8883b
Andrea <andreaskula92@gmail.com>
2018-09-04 18:04:49 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,7 @@
+# Design an algorithm that finds the maximum positive integer input by a user.  
+# The user repeatedly inputs numbers until a negative value is entered.
+
+
+# The program runs while user puts in positive numbers, 
+# When the user puts in a negative number the loop stops
+# The program then prints out the highest number the user entered
