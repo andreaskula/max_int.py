d5535101e0d98606342cdec13dc15b189ec4c2a2
Andrea <andreaskula92@gmail.com>
2018-09-04 18:35:03 +0000

    finito

--- max_int.py
+++ max_int.py
@@ -5,3 +5,18 @@
 # The program runs while user puts in positive numbers, 
 # When the user puts in a negative number the loop stops
 # The program then prints out the highest number the user entered
+
+num_int = 0
+max_int = 0
+
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int > max_int: 
+        max_int = num_int
+
+print("The maximum is", max_int)    # Do not change this line
+
+
+
+
